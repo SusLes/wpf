fe62a0d4884afa697ee7995e068f6f564da9db9c
Steven Kirbach <steven.kirbach@gmail.com>
2019-04-30 11:07:30 -0700

    echo path so we can see what's up

--- eng/helix/configure-helix-machine.ps1
+++ eng/helix/configure-helix-machine.ps1
@@ -3,9 +3,11 @@
 $dotnetLocation = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "dotnet"
 
 Write-Output "Adding dotnet location to path: $dotnetLocation"
+Write-Output "Path before: $env:path"
 # Prepend the path to ensure that dotnet.exe is called from there.
 $env:path="$dotnetLocation;$env:path"
 
+Write-Output "Path after: $env:path"
 # Don't look outside the TestHost to resolve best match SDKs.
 $env:DOTNET_MULTILEVEL_LOOKUP = 0
 
